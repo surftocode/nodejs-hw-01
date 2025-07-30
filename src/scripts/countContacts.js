eb67c7e48fc75acbad530261f6e0feadd7533093
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-07-30 14:52:34 +0300

    6. task tamamlndı

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,22 @@
-export const countContacts = async () => {};
+import fs from "fs/promises"
+import {PATH_DB} from "../constants/contacts.js"
 
-console.log(await countContacts());
+export const countContacts = async () => {
+
+try{
+    const data=await fs.readFile(PATH_DB,"utf-8");
+    const contacts=JSON.parse(data);
+    contacts.forEach((contact,index)=>{
+        const keyCount=Object.keys(contact).length;
+        console.log(`Kişi ${index+1}, ${keyCount} tane "key" içeriyor.`)
+    })
+
+
+}catch(err){
+    console.error("Cant found count: ",err.message)
+
+}
+
+};
+
+countContacts();
