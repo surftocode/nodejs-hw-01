bea85af0935a7c959aa4c8a3cc4d9d20c4ab6200
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-07-31 08:35:48 +0300

    last task added.

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -5,6 +5,7 @@ export const removeLastContact = async () => {
     const allContent = await fs.readFile(PATH_DB, 'utf-8');
     const data = JSON.parse(allContent);
     const deleteLastItem = data.slice(0, -1);
+    console.log(deleteLastItem);
   } catch (err) {
     console.error('Error accured:', err);
   }
