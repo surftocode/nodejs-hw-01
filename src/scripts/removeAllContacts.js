c50ca7e9d01d6ba7b403683552d460dca1eea895
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-07-31 08:02:14 +0300

    7th task is completed.

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,13 @@
-export const removeAllContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+
+export const removeAllContacts = async () => {
+  try {
+    const deletedFile = await fs.writeFile(PATH_DB, [],'utf-8');
+    console.log('All items are deleted succesfully!');
+  } catch (err) {
+    console.error('Cannot delete items:', err);
+  }
+};
 
 removeAllContacts();
